847b658a35284d238197c7897e456aea6bdeabd4
andy <andy@smokeybayair.com>
2016-04-07 21:47:15 -0800

    Up to date

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -19,7 +19,7 @@ var all = {
   root: path.normalize(__dirname + '/../../..'),
 
   // Server port
-  port: process.env.PORT || 9000,
+  port: 9000||process.env.PORT || 9000,
 
   // Server IP
   ip: '0.0.0.0'||process.env.IP || '0.0.0.0',
