c3c25a5bb48abc46a92190fde68878bdfffb4f21
andy <andy@smokeybayair.com>
2016-05-11 14:10:06 -0800

    total weights instead of payload

--- client/app/oneFlight/oneFlight.controller.js
+++ client/app/oneFlight/oneFlight.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tempApp')
-  .controller('OneFlightCtrl', function ($scope, $http, $interval, $q, uiGridConstants, tcFactory,Modal,$window,$timeout) {
+  .controller('OneFlightCtrl', function ($scope, $http, $interval, $q, tcFactory,Modal,$window,$timeout) {
     var aircraftSch, pilotSch ;
     this.arr=[];
     tcFactory.getAircraft(function(ac){
@@ -18,6 +18,8 @@ angular.module('tempApp')
     this.times = [];
     var date; 
     var smfltnum;
+    var ac;
+    var p;
     this.time={};
     this.time.selected={ref:parseInt(this.smfltnum,10),time:this.smfltnum + ":00"};
     for (var i=7;i<=19;i++){
@@ -130,142 +132,133 @@ angular.module('tempApp')
       tcFactory.setDate(this.date);
       body = {date:this.date, smfltnum:this.smfltnum+'A'};
       sections=[];
-    $http.post('/api/reservations/day', body).then(response => {
-      response.data.forEach(function(res){
-        if (!res.LAST) res.LAST="";
-        section=res['FLIGHT#'].substring(0,1);
-        flight=res['FLIGHT#'];
-        tc = res['Ref#'];
-        sectionIndex=-1;
-        flightIndex=-1;
-        tcIndex=-1;
-        for (var i=0;i<sections.length;i++){
-          if (sections[i].section===section) sectionIndex = i;
-        }
-        if (sectionIndex===-1) {
-          sections.push({section:section,flights:[]});
-          sections.sort(function(a,b){
-            return a.section.localeCompare(b.section);
-          });
+      $http.post('/api/reservations/day', body).then(response => {
+        response.data.forEach(function(res){
+          if (!res.LAST) res.LAST="";
+          section=res['FLIGHT#'].substring(0,1);
+          flight=res['FLIGHT#'];
+          tc = res['Ref#'];
+          sectionIndex=-1;
+          flightIndex=-1;
+          tcIndex=-1;
           for (var i=0;i<sections.length;i++){
-            if (sections[i].section===section) sectionIndex=i;
+            if (sections[i].section===section) sectionIndex = i;
           }
-        }
-        for (var i=0;i<sections[sectionIndex].flights.length;i++){
-          if (sections[sectionIndex].flights[i].flight.toUpperCase()===flight.toUpperCase()) flightIndex = i;
-        }
-        if (flightIndex===-1) {
-          sections[sectionIndex].flights.push({flight:flight,tcs:[]});
-          sections[sectionIndex].flights.sort(function(a,b){
-            return a.flight.toUpperCase().localeCompare(b.flight.toUpperCase());
-          });
+          if (sectionIndex===-1) {
+            sections.push({section:section,flights:[]});
+            sections.sort(function(a,b){
+              return a.section.localeCompare(b.section);
+            });
+            for (var i=0;i<sections.length;i++){
+              if (sections[i].section===section) sectionIndex=i;
+            }
+          }
           for (var i=0;i<sections[sectionIndex].flights.length;i++){
             if (sections[sectionIndex].flights[i].flight.toUpperCase()===flight.toUpperCase()) flightIndex = i;
           }
-          
-        }
-        for (var i=0;i<sections[sectionIndex].flights[flightIndex].tcs.length;i++){
-          if (sections[sectionIndex].flights[flightIndex].tcs[i].tc===tc) tcIndex = i;
-        }
-        if (tcIndex===-1) {
-          sections[sectionIndex].flights[flightIndex].tcs.push({tc:tc,reservations:[]});
-          sections[sectionIndex].flights[flightIndex].tcs.sort(function(a,b){
-            return a.tc>b.tc;
-          });
+          if (flightIndex===-1) {
+            sections[sectionIndex].flights.push({flight:flight,tcs:[]});
+            sections[sectionIndex].flights.sort(function(a,b){
+              return a.flight.toUpperCase().localeCompare(b.flight.toUpperCase());
+            });
+            for (var i=0;i<sections[sectionIndex].flights.length;i++){
+              if (sections[sectionIndex].flights[i].flight.toUpperCase()===flight.toUpperCase()) flightIndex = i;
+            }
+            
+          }
           for (var i=0;i<sections[sectionIndex].flights[flightIndex].tcs.length;i++){
             if (sections[sectionIndex].flights[flightIndex].tcs[i].tc===tc) tcIndex = i;
           }
-          
-        }
-        if (res['INVOICE#']) res['INVOICE#'] = res['INVOICE#'].substring(0,9);
-        sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.push(res);
-        //reservations[reservations.length-1].time set
-        var hr = body.smfltnum.substring(0,2);
-        var tvlC= sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations[sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.length-1]['Ref#'];
-        var after="";
-        switch (tvlC) {
-          case 1: after=":00";
-          break;
-          case 2: after=":00";
-          break;
-          case 3: after=":00";
-          break;
-          case 4: after=":15";
-          break;
-          case 5: after=":15";
-          break;
-          case 6: after=":25";
-          break;
-          case 7: after=":25";
-          break;
-          case 8: after=":25";
-          break;
-          case 9: after=":25";
-          break;
-          case 10: after=":25";
-          break;
-          case 11: after=":25";
-          break;
-          case 12: after=":40";
-          break;
-          default: after=":00";
-        }
-        sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations[sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.length-1].time = 
-            hr+after;
-      });
-      tcFactory.getData(function(tcs){
-        for (var i=0;i<sections.length;i++){
-          for (var j=0;j<sections[i].flights.length;j++){
-             //aircraft and pilot and total load HOM for sections[i].flights[j]
-             
-             for (var k=0;k<sections[i].flights[j].tcs.length;k++){
-               sections[i].flights[j].tcs[k].code = tcs.filter(function(element){
-                return element['Ref#']===sections[i].flights[j].tcs[k].reservations[0]['Ref#'];
-               })[0]['Route'];
-             }
+          if (tcIndex===-1) {
+            sections[sectionIndex].flights[flightIndex].tcs.push({tc:tc,reservations:[]});
+            sections[sectionIndex].flights[flightIndex].tcs.sort(function(a,b){
+              return a.tc>b.tc;
+            });
+            for (var i=0;i<sections[sectionIndex].flights[flightIndex].tcs.length;i++){
+              if (sections[sectionIndex].flights[flightIndex].tcs[i].tc===tc) tcIndex = i;
+            }
+            
           }
-        }
-      });
-      tcFactory.getFlights(body,function(flights){
-        for (var i=0;i<sections.length;i++){
-          sections[i].endTime = (parseInt(body.smfltnum.substring(0,2),10)+1).toString() + ':00';
-          for (var j=0;j<sections[i].flights.length;j++){
-             //aircraft and pilot and total load HOM for sections[i].flights[j]
-             var flight = flights.filter(function(flt){
-               return (flt['FLIGHT#'].toUpperCase()===sections[i].flights[j].flight.toUpperCase());
-             })[0];
-             if (flight) {
-               sections[i].flights[j].aircraft = flight.AIRCRAFT;
-               sections[i].flights[j].pilot = flight.PILOT;
-               sections[i].pilot = flight.PILOT;
-               sections[i].flights[j].date = flight.DATE;
-             }
-             sections[i].flights[j].total=0;
-             for (var k=0;k<sections[i].flights[j].tcs.length;k++){
-               for (var l=0;l<sections[i].flights[j].tcs[k].reservations.length;l++){
-                 var r = sections[i].flights[j].tcs[k].reservations[l]['Ref#'];
-                 if ((r>0&&r<4)||(r>9&&r<13)) {
-                   sections[i].flights[j].total += sections[i].flights[j].tcs[k].reservations[l]['WEIGHT'] + sections[i].flights[j].tcs[k].reservations[l]['FWeight'];
+          if (res['INVOICE#']) res['INVOICE#'] = res['INVOICE#'].substring(0,9);
+          sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.push(res);
+          //reservations[reservations.length-1].time set
+          var hr = body.smfltnum.substring(0,2);
+          var tvlC= sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations[sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.length-1]['Ref#'];
+          var after=[":00",":00",":00",":00",":15",":15",":25",":25",":25",":25",":25",":25",":40"];
+          if (tvlC>12) tvlC=0;
+          sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations[sections[sectionIndex].flights[flightIndex].tcs[tcIndex].reservations.length-1].time = 
+              hr+after[tvlC];
+        });
+        tcFactory.getData(function(tcs){
+          for (var i=0;i<sections.length;i++){
+            for (var j=0;j<sections[i].flights.length;j++){
+               //aircraft and pilot and total load HOM for sections[i].flights[j]
+               
+               for (var k=0;k<sections[i].flights[j].tcs.length;k++){
+                 sections[i].flights[j].tcs[k].code = tcs.filter(function(element){
+                  return element['Ref#']===sections[i].flights[j].tcs[k].reservations[0]['Ref#'];
+                 })[0]['Route'];
+               }
+            }
+          }
+        });
+        tcFactory.getFlights(body,function(flights){
+          for (var i=0;i<sections.length;i++){
+            sections[i].endTime = (parseInt(body.smfltnum.substring(0,2),10)+1).toString() + ':00';
+            for (var j=0;j<sections[i].flights.length;j++){
+               //aircraft and pilot and total load HOM for sections[i].flights[j]
+               var flight = flights.filter(function(flt){
+                 return (flt['FLIGHT#'].toUpperCase()===sections[i].flights[j].flight.toUpperCase());
+               })[0];
+               if (flight) {
+                 sections[i].flights[j].aircraft = flight.AIRCRAFT;
+                 sections[i].flights[j].pilot = flight.PILOT;
+                 sections[i].pilot = flight.PILOT;
+                 sections[i].flights[j].date = flight.DATE;
+               }
+               sections[i].flights[j].total=180;//30 gallons fuel as base amount
+               ac = aircraftSch.filter(function(a){
+                 return a.Aircraft===flight.AIRCRAFT;
+               })
+               if (ac.length>0) sections[i].flights[j].total += ac[0]['Max Load'];
+               else sections[i].flights[j].total=-9999;
+               p = pilotSch.filter(function(a){
+                 return a.Pilot===flight.PILOT;
+               })
+               if (p.length>0) sections[i].flights[j].total += p[0]['Weight'];
+               else sections[i].flights[j].total=-9999;
+               sections[i].flights[j].keb=sections[i].flights[j].total;
+               var keb=0;
+               for (var k=0;k<sections[i].flights[j].tcs.length;k++){
+                 for (var l=0;l<sections[i].flights[j].tcs[k].reservations.length;l++){
+                   var r = sections[i].flights[j].tcs[k].reservations[l]['Ref#'];
+                   if ((r>0&&r<4)||(r>9&&r<13)) {
+                     sections[i].flights[j].total += sections[i].flights[j].tcs[k].reservations[l]['WEIGHT'] + sections[i].flights[j].tcs[k].reservations[l]['FWeight'];
+                   }
+                   if (r===6||r===9||r===10) {
+                     keb += sections[i].flights[j].tcs[k].reservations[l]['WEIGHT'] + sections[i].flights[j].tcs[k].reservations[l]['FWeight'];
+                   }
                  }
                }
-             }
-          }
-        }  
-        tcFactory.getPilots(function(pilotSch){
-          for (var i=0;i<sections.length;i++){
-            var p = pilotSch.filter(function(pilot){
-              if (!sections[i].pilot) return false;
-              return pilot.Pilot.toUpperCase()===sections[i].pilot.toUpperCase();
-            });
-            if (p.length>0) sections[i].pilotCert = p[0].lic;
+               if (keb>0) sections[i].flights[j].keb += keb;
+               else sections[i].flights[j].keb = 0;
+            }
           }  
-          
+          tcFactory.getPilots(function(pilotSch){
+            for (var i=0;i<sections.length;i++){
+              var p = pilotSch.filter(function(pilot){
+                if (!sections[i].pilot) return false;
+                return pilot.Pilot.toUpperCase()===sections[i].pilot.toUpperCase();
+              });
+              if (p.length>0) sections[i].pilotCert = p[0].lic;
+            }  
+            
+          });
         });
+        this.sections=sections;
+        enterTimes(response.data); 
+        $timeout($window.print,0);
       });
-      this.sections=sections;
-      enterTimes(response.data); 
-      $timeout($window.print,0);
-    });
     };//end of this.print
     
     var enterTimes = function(reservations){
@@ -298,8 +291,6 @@ angular.module('tempApp')
           else {
             //remove spliced flight from database
             $http.delete('/api/flights/' + allFlights[i]._id).then(function(res){
-              console.log('deleted ' )
-              console.log(allFlights[i])
             });
             allFlights.splice(i,1);
             
@@ -336,7 +327,6 @@ angular.module('tempApp')
             if (success) {
               //we have found our routing for this section!
               //write appropriate flight times for the found possibility  
-              console.log(possibilities[i]);// + ' for section ' + flight['FLIGHT#'].substring(0,1));
               otherFlight = allFlights.filter(function(f){
                 return f['FLIGHT#'].toUpperCase()===flight['FLIGHT#'].substring(0,3) + 'B';
               });
@@ -373,7 +363,6 @@ angular.module('tempApp')
                   }
                 }
               }
-              console.log(flight)
               //save changes
               $http.put('/api/flights/'+ flight._id,flight);
               if (otherFlightUsed) $http.put('/api/flights/'+ otherFlight._id,otherFlight);
