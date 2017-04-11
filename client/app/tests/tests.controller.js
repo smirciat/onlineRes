149c4a8d110c6cc2046febcd0bfc4888a6f33970
andy <andy@smokeybayair.com>
2017-04-10 17:05:55 -0800

    add test user modal

--- client/app/tests/tests.controller.js
+++ client/app/tests/tests.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('tempApp')
-  .controller('TestsCtrl', function ($scope,appConfig,Auth,$sce) {
+  .controller('TestsCtrl', function ($scope,appConfig,Auth,$sce,Modal) {
+    this.quickMessage=Modal.confirm.quickMessage();
+    this.quickMessage("If the test initializes with the wrong name, please select another test from the pulldown menu, then immediately re-select this test.  If you start a test, please finish it before moving on.");
     var user = Auth.getCurrentUser;
     var url = "https://www.classmarker.com/online-test/start/?quiz=" 
       + appConfig.tests[0].id 
